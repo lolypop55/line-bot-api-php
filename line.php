75ed9edc407a9a02b89044c0b544ce666f18f937
Pongtud Bualerd <gust4evergust@gmail.com>
2019-10-27 14:20:27 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'Vrt2nU13R4ucf2xg6WN5n4Z9+qO4ADuB6GcruFri1Ix3I8gb6MKLBRhQNjgPgJfhssqJcbco3DBJlfQFiP6ryEBlN2ufr552CJyYJWIwdo2fFEfL0TB/mYpTebxOsmwr4EvpUjRpdYAws1WpD2fbOgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'M16K0NniHwUn2aLbZMwJDDLeax7WZ6ejHkf/LJ+u6kMMqLhNL2hBhraDnqSrvwEwxAHQEv76/AujfudcSq8jFd8v+3cf9i0m/VeOM5o1ZNAOf6WsrrL+MnjLWdg/wuYi7M9qgFDus1LM6sJfbM7NtAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
